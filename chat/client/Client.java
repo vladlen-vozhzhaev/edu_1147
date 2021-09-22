b7d809b9fb74382d19d17208a2e5bb0ee426fdae
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2021-09-22 19:17:46 +0300

    Create Client.java

---
+++ chat/client/Client.java
@@ -0,0 +1,43 @@
+package client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            Socket socket = new Socket("127.0.0.1", 8188);
+            System.out.println("Успешно подключен!");
+            // Поток ввода
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            // Поток вывода
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true){
+                            String response = in.readUTF();
+                            System.out.println("Ответ сервера - "+response);
+                        }
+                    }catch (IOException e){
+                        System.out.println("Соединение разорвано");
+                    }
+                }
+            });
+            thread.start();
+
+            while (true){
+                System.out.println("Введите сообщение: ");
+                String text = scanner.nextLine();
+                out.writeUTF(text);
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
