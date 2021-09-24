74ca082cf34deaf38bd4e43ff1deca400abe0507
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2021-09-24 14:01:24 +0300

    Update Server.java

--- chat/server/Server.java
+++ chat/server/Server.java
@@ -9,42 +9,62 @@ import java.util.ArrayList;
 
 public class Server {
     public static void main(String[] args) {
-        ArrayList<Socket> usersSocket = new ArrayList<>();
+        ArrayList<User> users = new ArrayList<>();
         try {
             ServerSocket serverSocket = new ServerSocket(8188);
             System.out.println("Сервер запущен");
             while (true){
                 Socket socket = serverSocket.accept();
                 System.out.println("Подключился новый клиент");
-                usersSocket.add(socket);
-                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                DataInputStream in = new DataInputStream(socket.getInputStream());
+                User currentUser = new User(socket);
+                users.add(currentUser);
+                currentUser.setOut(new DataOutputStream(currentUser.getSocket().getOutputStream()));
+                currentUser.setIn(new DataInputStream(currentUser.getSocket().getInputStream()));
                 Thread thread = new Thread(new Runnable() {
                     @Override
                     public void run() {
                         String text;
                         try {
+                            currentUser.getOut().writeUTF("Введите имя: ");
+                            String userName = currentUser.getIn().readUTF();
+                            currentUser.setUserName(userName);
+                            for (User user : users) {
+                                user.getOut().writeUTF("Пользователь "+currentUser.getUserName()+" присоеденился к беседе");
+                            }
+
                             while (true) {
-                                text = in.readUTF();
-                                System.out.println("Клиент прислал сообщение: " + text);
-                                for (Socket userSocket : usersSocket) {
-                                    DataOutputStream out = new DataOutputStream(userSocket.getOutputStream());
-                                    out.writeUTF(text);
+                                text = currentUser.getIn().readUTF();
+                                if(text.equals("/onlineUsers")){
+                                    String names = "Пользователи онлайн: ";
+                                    for (User user: users){
+                                        names += user.getUserName()+", ";
+                                    }
+                                    currentUser.getOut().writeUTF(names); // Отправили список
+                                }else{
+                                    // Рассылка сообщения
+                                    System.out.println(currentUser.getUserName()+": " + text);
+                                    for (User user : users) {
+                                        if (currentUser.getUuid().equals(user.getUuid())) continue;
+                                        user.getOut().writeUTF(currentUser.getUserName()+": " + text);
+                                    }
                                 }
+
                             }
                         } catch (IOException exception) {
-                            usersSocket.remove(socket);
-                            System.out.println("Клиент отключился");
+                            users.remove(currentUser);
+                            for (User user : users) {
+                                try {
+                                    user.getOut().writeUTF("Пользователь "+currentUser.getUserName()+" покинул беседу");
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            System.out.println("Пользователь "+currentUser.getUserName()+" покинул беседу");
                         }
                     }
                 });
                 thread.start();
             }
-
-
-
-
-
         } catch (IOException exception) {
             exception.printStackTrace();
         }
