5c2a6ea0a419ad0feaeb27197d4671a146d1b518
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2021-09-24 14:02:28 +0300

    Update Client.java

--- chat/client/Client.java
+++ chat/client/Client.java
@@ -22,7 +22,7 @@ public class Client {
                     try {
                         while (true){
                             String response = in.readUTF();
-                            System.out.println("Ответ сервера - "+response);
+                            System.out.println(response);
                         }
                     }catch (IOException e){
                         System.out.println("Соединение разорвано");
@@ -32,7 +32,6 @@ public class Client {
             thread.start();
 
             while (true){
-                System.out.println("Введите сообщение: ");
                 String text = scanner.nextLine();
                 out.writeUTF(text);
             }
