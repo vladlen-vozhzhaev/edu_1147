5f81ac841923397ba38efe2e84980d522ec020ec
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2021-09-30 10:01:47 +0300

    Update User.java

--- chat/server/User.java
+++ chat/server/User.java
@@ -2,15 +2,17 @@ package server;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.UUID;
 
-public class User {
+public class User{
     private Socket socket;
     private String userName;
     private UUID uuid;
     private DataOutputStream out;
     private DataInputStream in;
+    ObjectOutputStream oos;
 
     public User(Socket socket) {
         this.socket = socket;
@@ -22,8 +24,10 @@ public class User {
     public UUID getUuid() { return uuid; }
     public DataOutputStream getOut() { return out; }
     public DataInputStream getIn() { return in; }
+    public ObjectOutputStream getOos(){return oos;}
 
     public void setUserName(String userName) { this.userName = userName; }
     public void setOut(DataOutputStream out) { this.out = out; }
     public void setIn(DataInputStream in) { this.in = in; }
+    public void setOos(ObjectOutputStream oos){this.oos = oos;}
 }
