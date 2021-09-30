429ef7a169ce125ecdc2da52466b0061d67967d4
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2021-09-30 10:01:31 +0300

    Update Server.java

--- chat/server/Server.java
+++ chat/server/Server.java
@@ -3,6 +3,7 @@ package server;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ import java.util.ArrayList;
 public class Server {
     public static void main(String[] args) {
         ArrayList<User> users = new ArrayList<>();
+        ArrayList<String> usersName = new ArrayList<>();
         try {
             ServerSocket serverSocket = new ServerSocket(8188);
             System.out.println("Сервер запущен");
@@ -18,18 +20,21 @@ public class Server {
                 System.out.println("Подключился новый клиент");
                 User currentUser = new User(socket);
                 users.add(currentUser);
-                currentUser.setOut(new DataOutputStream(currentUser.getSocket().getOutputStream()));
+                currentUser.setOos(new ObjectOutputStream(currentUser.getSocket().getOutputStream()));
                 currentUser.setIn(new DataInputStream(currentUser.getSocket().getInputStream()));
                 Thread thread = new Thread(new Runnable() {
                     @Override
                     public void run() {
                         String text;
                         try {
-                            currentUser.getOut().writeUTF("Введите имя: ");
-                            String userName = currentUser.getIn().readUTF();
+                            currentUser.getOos().writeObject("Введите имя: "); // Отправка клиенту
+                            String userName = currentUser.getIn().readUTF(); // Получаем от клиента
                             currentUser.setUserName(userName);
+                            usersName.add(userName);
+
                             for (User user : users) {
-                                user.getOut().writeUTF("Пользователь "+currentUser.getUserName()+" присоеденился к беседе");
+                                user.getOos().writeObject(new ArrayList<>(usersName)); // Отправка клиенту
+                                user.getOos().writeObject("Пользователь "+currentUser.getUserName()+" присоеденился к беседе");
                             }
 
                             while (true) {
@@ -39,22 +44,32 @@ public class Server {
                                     for (User user: users){
                                         names += user.getUserName()+", ";
                                     }
-                                    currentUser.getOut().writeUTF(names); // Отправили список
+                                    currentUser.getOos().writeObject(names); // Отправили список
+                                }else if(text.indexOf("/m") == 0){
+                                    String[] splittedText = text.split(" ");
+                                    for(User user : users){
+                                        if(user.getUserName().equals(splittedText[1])){
+                                            user.getOos().writeObject(splittedText[2]);
+                                            break;
+                                        }
+                                    }
                                 }else{
                                     // Рассылка сообщения
                                     System.out.println(currentUser.getUserName()+": " + text);
                                     for (User user : users) {
                                         if (currentUser.getUuid().equals(user.getUuid())) continue;
-                                        user.getOut().writeUTF(currentUser.getUserName()+": " + text);
+                                        user.getOos().writeObject(currentUser.getUserName()+": " + text);
                                     }
                                 }
 
                             }
                         } catch (IOException exception) {
-                            users.remove(currentUser);
+                            users.remove(currentUser); // Удалить User
+                            usersName.remove(currentUser.getUserName()); // Удалить имя из списка
                             for (User user : users) {
                                 try {
-                                    user.getOut().writeUTF("Пользователь "+currentUser.getUserName()+" покинул беседу");
+                                    user.getOos().writeObject(new ArrayList<>(usersName));
+                                    user.getOos().writeObject("Пользователь "+currentUser.getUserName()+" покинул беседу");
                                 } catch (IOException e) {
                                     e.printStackTrace();
                                 }
